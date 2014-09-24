bfe8b1c0b6818e5559eee5e55bd118ab98bb8d30
Gio Peralto-Pritchard <gpera008@fiu.edu>
2014-09-24 17:27:08 -0400

    views users

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -143,22 +143,12 @@ var MainController = {
 		var password 		= req.param('password');
 
 		// FINDS ALL USERS
-		User.findOneByEmail('amarin-stoddart@acm.com')
+		User.find()
 		.done(function(err,found) {
 			if(err) {
 				console.log("Error!");
 			} else if(found) {
 				console.log(found);
-				User.destroy({email: 'amarin-stoddart@acm.com'})
-				.exec(function deleteDeleteStory(error, user) {
-					if(error) {
-						// Set the error header
-	        			res.set('error', 'DB Error');
-	        			res.send(500, { error: "DB Error" });
-					} else {
-						res.send(user);
-					}
-				});
 			}
 			else {
 				console.log("Nothing found!");
